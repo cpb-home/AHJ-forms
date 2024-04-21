9fc4d1f347822dbef12f2dab6a2c5a93d7401455
cpb-home <cpb@bizb.ru>
2024-04-21 18:36:42 +0300

    test done

--- e2e/e2e.test.js
+++ e2e/e2e.test.js
@@ -1,9 +1,9 @@
-import puppetteer from 'puppeteer';
+import puppeteer from 'puppeteer';
 import { fork } from 'child_process';
 
 jest.setTimeout(30000); // default puppeteer timeout
 
-describe('Credit Card Validator form', () => {
+describe('Testing of blocks', () => {
   let browser = null;
   let page = null;
   let server = null;
@@ -20,7 +20,7 @@ describe('Credit Card Validator form', () => {
       });
     });
 
-    browser = await puppetteer.launch({
+    browser = await puppeteer.launch({
       // headless: false, // show gui
       // slowMo: 250,
       // devtools: true, // show devTools
@@ -28,12 +28,22 @@ describe('Credit Card Validator form', () => {
     page = await browser.newPage();
   });
 
+  test('should render the page and make click to button for modal appear', async () => {
+    await page.goto(baseUrl); 
+    
+    await page.waitForSelector('body');
+
+    await page.waitForSelector('.container');
+
+    const buttons = await page.$('button');
+
+    await buttons.click();
+    
+    expect(await page.waitForSelector('.tip')).toBeTruthy();
+  });
+
   afterAll(async () => {
     await browser.close();
     server.kill();
-  });
-
-  test('should add do something', async () => {
-    await page.goto(baseUrl);
   });
 });
